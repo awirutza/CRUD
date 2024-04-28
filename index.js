3478f1d087ae0e3aa1eaa7ee46eb5360adf6e52d
Krit <115700794+awirutza@users.noreply.github.com>
2024-04-28 10:25:05 +0700

    เชื่อมต่อฐานข้อมูล

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ const mysql = require('mysql2')
 const app = express()
 const port = 3000
 
-function ConnectToDatabase(){
-    const connection =  mysql.createConnection({
-        host:'',
-        user:'',
-        password:'',
-        database:''
+require('dotenv').config();
+const {MyHost,MyUser,MyPassword,MyDatabase} = process.env;
+
+
+const connection =  mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'usermanagement'
+})
+
+//ตรวจสอบ Conection ว่าเชื่อมต่อได้หรือไม่
+    connection.connect((err) =>{
+        if(err){
+            console.log("Erorr conection to Database");
+            console.log(err)
+        }else{
+            console.log('Database is conected successfully !');
+        }
     })
-}
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const SelectDataSQL = 'SELECT * FROM user'
+    connection.query(SelectDataSQL,(err,result) =>{
+        if(err){
+            console.log("การดึงข้อมูลผิดพลาด");
+        }else{
+            console.log("เลือกข้อมูลเรียบร้อยแล้ว");
+            res.send('Hello World!');
+            connection.end();        
+        }
+    })
 })
 
 app.listen(port, () => {
